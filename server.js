397b9a9a4e84541c5c99ffab2b1144cecfc80803
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-30 18:29:49 +0800

    用 script 发请求

--- server.js
+++ server.js
@@ -41,15 +41,13 @@ var server = http.createServer(function(request, response){
   }else if(path === '/pay'){
     var amount = fs.readFileSync('./db', 'utf8') // 100
     var newAmount = amount - 1
-    if(Math.random()>0.5){
-      fs.writeFileSync('./db', newAmount)
-      response.statusCode = 200
-      response.write(fs.readFileSync('./img/1.jpg'))
-    }else{
-      response.statusCode = 400
-      response.write('fail')
-    }
-     response.end()
+    fs.writeFileSync('./db', newAmount)
+    response.setHeader('Content-Type', 'application/javascript')
+    response.statusCode = 200
+    response.write(`
+      amount.innerText = amount.innerText - 1
+    `)
+    response.end()
   }else{
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
